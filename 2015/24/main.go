6a2d31b7005eb1fb28629103cfbc3d692654cb67
D. Corto <5486937+dcorto@users.noreply.github.com>
2025-02-10 11:49:46 +0100

    Added days 22,23,24,25 as placeholders for now

---
+++ 2015/24/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+const day = 24
+
+func main() {
+	fmt.Println("Solution for Day", day)
+
+	startTimeA := time.Now()
+	solutionA := solutionA()
+	fmt.Println("Solution A:", solutionA, "(Time:", time.Since(startTimeA), ")")
+
+	startTimeB := time.Now()
+	solutionB := solutionB()
+	fmt.Println("Solution B:", solutionB, "(Time:", time.Since(startTimeB), ")")
+}
+
+func solutionA() int {
+	var solution = 0
+	return solution
+}
+
+func solutionB() int {
+	var solution = 0
+	return solution
+}
