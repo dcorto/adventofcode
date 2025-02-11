b25220cb8e3ea5725db43c47896d9b64c11294dc
D. Corto <5486937+dcorto@users.noreply.github.com>
2025-02-11 11:42:23 +0100

    Day 23 2015

--- 2015/23/main.go
+++ 2015/23/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 	"time"
+	"utils"
 )
 
 const day = 23
@@ -21,10 +24,81 @@ func main() {
 
 func solutionA() int {
 	var solution = 0
+
+	instructions, err := utils.ReadLinesFromFile(fmt.Sprintf("%d/input.txt", day))
+	if err != nil {
+		fmt.Println("Error:", err)
+		return solution
+	}
+
+	registers := map[string]int{
+		"a": 0,
+		"b": 0,
+	}
+
+	run(instructions, registers)
+
+	solution = registers["b"]
 	return solution
 }
 
 func solutionB() int {
 	var solution = 0
+
+	instructions, err := utils.ReadLinesFromFile(fmt.Sprintf("%d/input.txt", day))
+	if err != nil {
+		fmt.Println("Error:", err)
+		return solution
+	}
+
+	registers := map[string]int{
+		"a": 1, // Part 2 only change
+		"b": 0,
+	}
+
+	run(instructions, registers)
+
+	solution = registers["b"]
 	return solution
+}
+
+func run(instructions []string, registers map[string]int) {
+	i := 0
+	for i < len(instructions) {
+		parts := strings.SplitN(instructions[i], " ", 2)
+		cmd := parts[0]
+		param := parts[1]
+		switch cmd {
+		case "hlf":
+			registers[param] = registers[param] / 2
+			i++
+		case "tpl":
+			registers[param] *= 3
+			i++
+		case "inc":
+			registers[param]++
+			i++
+		case "jmp":
+			offset, _ := strconv.Atoi(param)
+			i += offset
+		case "jie":
+			params := strings.Split(param, ", ")
+			p1 := params[0]
+			offset, _ := strconv.Atoi(params[1])
+			if registers[p1]%2 == 0 {
+				i += offset
+			} else {
+				i++
+			}
+		case "jio":
+			params := strings.Split(param, ", ")
+			p1 := params[0]
+			offset, _ := strconv.Atoi(params[1])
+			if registers[p1] == 1 {
+				i += offset
+			} else {
+				i++
+			}
+		}
+	}
 }
