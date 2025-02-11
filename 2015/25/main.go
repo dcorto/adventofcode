0d5197564977afc34cd9cfabfe9417ca85577889
D. Corto <5486937+dcorto@users.noreply.github.com>
2025-02-11 17:57:15 +0100

    Day 25 2015 (part 1)

--- 2015/25/main.go
+++ 2015/25/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"time"
+	"utils"
 )
 
 const day = 25
@@ -21,10 +23,43 @@ func main() {
 
 func solutionA() int {
 	var solution = 0
+
+	data, err := utils.ReadFromFile(fmt.Sprintf("%d/input.txt", day))
+	if err != nil {
+		fmt.Println("Error:", err)
+		return solution
+	}
+
+	slit := strings.Split(data, ",")
+	row := utils.Atoi(slit[0])
+	col := utils.Atoi(slit[1])
+
+	// initial code
+	code := 20151125
+
+	// constants
+	const multiplier = 252533
+	const mod = 33554393
+
+	// Calculate the position in the sequence
+	position := calculatePosition(row, col)
+
+	// Generate the code in the given position
+	for i := 1; i < position; i++ {
+		code = (code * multiplier) % mod
+	}
+	solution = code
 	return solution
 }
 
 func solutionB() int {
 	var solution = 0
 	return solution
+}
+
+// calculatePosition calculate the position in the cell (row, col)
+func calculatePosition(row, col int) int {
+	// the position in the sequence is calculated by adding the numbers of the diagonal
+	diagonal := row + col - 1
+	return (diagonal*(diagonal-1))/2 + col
 }
