b62b8f52eb2296f67cc8f3c2867630f3155cbd2a
D. Corto <5486937+dcorto@users.noreply.github.com>
2025-05-05 10:52:45 +0200

    Day 9 (part B) Year 2016 solved

--- 2016/9/main.go
+++ 2016/9/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"strconv"
 	"strings"
@@ -54,5 +55,57 @@ func solutionA() int {
 
 func solutionB() int {
 	var solution = 0
+	data, err := utils.ReadFromFile(fmt.Sprintf("%d/input.txt", day))
+	if err != nil {
+		fmt.Println("Error:", err)
+		return solution
+	}
+	dataBytes := []byte(data) // Work with byte slices
+	solution = calculateDecompressedLengthV2(dataBytes)
+
 	return solution
+}
+
+// calculateDecompressedLengthV2 calculates length recursively for Part B
+func calculateDecompressedLengthV2(data []byte) int {
+	length := 0
+	i := 0
+	for i < len(data) {
+		if data[i] == '(' {
+			markerEndInSubstring := bytes.IndexByte(data[i:], ')')
+			// Assuming valid input, markerEndInSubstring will not be -1
+			markerEnd := i + markerEndInSubstring
+
+			marker := string(data[i+1 : markerEnd])
+			parts := strings.Split(marker, "x")
+			// Assuming valid input, len(parts) == 2 and Atoi succeeds
+
+			chars, _ := strconv.Atoi(parts[0])
+			repeat, _ := strconv.Atoi(parts[1])
+
+			// Define the segment that needs recursive decompression
+			segmentStart := markerEnd + 1
+			segmentEnd := segmentStart + chars
+			// Boundary check (important!)
+			if segmentEnd > len(data) {
+				segmentEnd = len(data)
+			}
+			segment := data[segmentStart:segmentEnd]
+
+			// Recursively calculate the length of the segment
+			recursiveLength := calculateDecompressedLengthV2(segment)
+
+			// Add the repeated length to the total
+			length += recursiveLength * repeat
+
+			// Advance the index past the marker and the segment it referred to
+			i = segmentEnd
+
+		} else {
+			// Regular character, just count it
+			length++
+			i++
+		}
+	}
+	return length
 }
